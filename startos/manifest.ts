2e16fb3e028c60e624dd3802d82a2c92d9aa6910
Matt Hill <MattDHill@users.noreply.github.com>
2024-08-09 08:09:53 -0600

    Initialize update/040

---
+++ startos/manifest.ts
@@ -0,0 +1,41 @@
+import { setupManifest } from '@start9labs/start-sdk'
+
+/**
+ * Here we define static properties of the package to be displayed in the Marketplace and used by StartOS.
+ */
+export const manifest = setupManifest({
+  id: 'hello-world',
+  title: 'Hello World',
+  version: '0.3.6:0',
+  releaseNotes: 'Revamped for StartOS 0.3.6',
+  license: 'mit',
+  wrapperRepo: 'https://github.com/Start9Labs/hello-world-wrapper',
+  upstreamRepo: 'https://github.com/Start9Labs/hello-world',
+  supportSite: 'https://docs.start9.com/',
+  marketingSite: 'https://start9.com/',
+  donationUrl: 'https://donate.start9.com/',
+  description: {
+    short: 'Bare bones example of a StartOS service',
+    long: 'Hello World is a template service that provides examples of basic StartOS features.',
+  },
+  assets: [], // directories of static files you want to mount to your container
+  volumes: ['main'], // IDs of persistence volumes that will be mounted to your container
+  images: {
+    main: {
+      source: {
+        dockerTag: 'start9/hello-world',
+      },
+    },
+  },
+  hardwareRequirements: {},
+  // optional alerts to display at different time in the service lifecycle
+  alerts: {
+    install: 'Optional alert to display before installing the service',
+    update: null,
+    uninstall: null,
+    restore: null,
+    start: null,
+    stop: null,
+  },
+  dependencies: {}, // See Hello Moon for an example with dependencies
+})
